523d65ce3c67daa3f523e9f27e4d8603488bdc33
Clayton Collins and Keith Evans <keith@>
2017-01-24 12:00:14 -0800

    add css flavor

--- js/scripts.js
+++ js/scripts.js
@@ -20,23 +20,49 @@ Ticket.prototype.price = function() {
 }
 
 Ticket.prototype.display = function() {
-  $('.result').append("<div class='ticket'>"+this.movie+"<br>"+this.time+"<br>"+this.age+"<br>$"+this.price()+"</div>")
+  $('.result').append("<div class='ticket'><p>"+this.movie.toUpperCase()+"<br>"+this.time.toUpperCase()+"<br>"+this.age.toUpperCase()+"<br>$"+this.price()+"</p></div>");
+  $('.movietime').fadeIn(1200);
 }
+
 
 
 
 /////////////////// FRONT END LOGIC
 
+
+
 $(function(){
+  $('.age').change(function () {
+    $('.age').hide();
+    $('.movie').show();
+    $('.speech').text("MMMMM. AND WHAT MOVIE DO YOU WANT TO SEE?")
+  });
+
+  $('.movie').change(function () {
+    $('.movie').hide();
+    $('.time').show();
+    $('.speech').text("THAT'S NICE. WHAT TIME?")
+  });
+
+  $('.time').change(function () {
+    $('.time').hide();
+    $('.btn').show();
+    $('.speech').text("OK, SEE YOU INSIDE.");
+  });
+
+  $('.movietime').click(function() {
+    $(this).animate({ marginLeft: "-100vw"}, 2000);
+  });
+
   $('.selectors').submit(function(event) {
     event.preventDefault();
+
+    $('.btn').hide();
     var age = $('.age').val();
     var movie = $('.movie').val();
     var time = $('.time').val();
-
     var newTicket = new Ticket(age,movie,time);
-    // var newPrice = newTicket.price();
-    newTicket.display();
 
-  })
-})
+    newTicket.display();
+  });
+});
